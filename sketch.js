8421eb083c180d0abb09f98a220c5d6813dee92c
SlimSpeedster <yashdgandhi@outlook.com>
2021-03-24 17:59:26 -0400

    learned databases

--- sketch.js
+++ sketch.js
@@ -1,9 +1,17 @@
 var ball;
+var database;
+var position;
 
 function setup(){
     createCanvas(500,500);
-    ball = createSprite(250,250,10,10);
-    ball.shapeColor = "red";
+    //calling the firebase database
+        database = firebase.database();  
+        ball = createSprite(250,250,10,10);
+        ball.shapeColor = "red";
+
+position = database.ref('ball/position');
+    //observes if there is a change in the database
+        position.on('value', readPosition, showError);
 }
 
 function draw(){
@@ -21,9 +29,26 @@ function draw(){
         changePosition(0,+1);
     }
     drawSprites();
-}
+}  
 
 function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+    database.ref('ball/position').set({
+        'x' : position.x + x,
+        'y': position.y + y
+    })
+    
+}
+
+function readPosition(data){
+    position = data.val();
+    ball.x = position.x;
+    ball.y = position.y;
+    
+}
+
+function showError(){
+
+    console.log("ERROR! GO FIX IT!")
+
+
 }
